f16bc104286cb0771a011279ea6e37c89c9c437c
Krish2Ski <seth9240@hallcrossacademy.co.uk>
2024-05-23 13:06:16 +0100

    made final grid

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-# Hello
-# Krish
-# Mr Liu
-print("asfdsaf")
 
-grid = ['T','O','P','S','E']
+grid = [['T','O','P','S','E'],
 
-grid2 = ['C','R','E','T','M']
+ ['C','R','E','T','M'],
 
-row3 = ['E','S','S','A','G']
+ ['E','S','S','A','G'],
 
-row4 = ['E','Y','R','P','L']
+ ['E','Y','R','P','L'],
+    
+ ['U','O','G','G','Q']]
 
-row5 = ['U','O','G','G','Q']
+print(grid)
